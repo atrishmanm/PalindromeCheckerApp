7be90b6ee14b8c5626a9116bd453c6fe9e05989d
atrishmanm <atrishmanm@gmail.com>
2026-02-27 11:08:59 +0530

    Added files to feature/UC9 branch

---
+++ src/PalindromeCheckerApp9.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class PalindromeCheckerApp9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+                String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        boolean result = isPalindrome(normalized);
+
+        System.out.println("Is Palindrome? : " + result);
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        if (str.length() <= 1) {
+            return true;
+        }
+
+        if (str.charAt(0) == str.charAt(str.length() - 1)) {
+            return isPalindrome(str.substring(1, str.length() - 1));
+        }
+
+        return false;
+    }
+}
