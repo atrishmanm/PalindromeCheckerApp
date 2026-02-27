fcd6c7bd6df935c9700e033c2439e06a1fca7818
atrishmanm <atrishmanm@gmail.com>
2026-02-27 11:01:17 +0530

    Added files to feature/UC2 branch

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,19 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
 
-  public static void main(String[] args) {
-    String input = "madam";
+        String input = "madam";
 
-    String reversed = "";
+        boolean isPalindrome = true;
 
-    for (int i = input.length() - 1; i >= 0; i--) {
-      reversed = reversed + input.charAt(i);
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
-
-    boolean isPalindrome = input.equals(reversed);
-
-    System.out.println("Input text: " + input);
-    System.out.println("Reversed text: " + reversed);
-    System.out.println("Is it a Palindrome? : " + isPalindrome);
-  }
 }
