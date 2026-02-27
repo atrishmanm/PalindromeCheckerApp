5570a333228d916b7ca89cf5b33e9fba7faaaf91
atrishmanm <atrishmanm@gmail.com>
2026-02-27 11:10:12 +0530

    Added files to feature/UC9 branch

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PalindromeCheckerApp9 {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
